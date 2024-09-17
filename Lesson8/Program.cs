ce3ad6f8117cc3e0285878e8e9bbbca2343d35aa
dia_zx <dia_zx@mail.ru>
2024-09-17 19:58:13 +0300

    done

---
+++ Lesson8/Program.cs
@@ -0,0 +1,15 @@
+﻿using Lesson8.Models;
+using Lesson8.Presenters;
+using Lesson8.Views;
+
+namespace Lesson8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new RestaurantPresenter(new Restaurant(), new RestaurantView()).Run();
+
+        }
+    }
+}
