899d783a420ec7da493b2971d92f15f4ce6a163c
dia_zx <dia_zx@mail.ru>
2024-09-20 00:29:13 +0300

    done

---
+++ Lesson9/Servises/IInMemoryToDoCollection.cs
@@ -0,0 +1,11 @@
+﻿using Lesson9.Models;
+
+namespace Lesson9.Services;
+
+public interface IInMemoryToDoCollection
+{
+    public IEnumerable<ToDoItem> GetAll();
+    public void Add(ToDoItem toDoItem);
+    public bool Remove(int id);
+}
+
