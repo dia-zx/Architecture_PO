825668a0b20c23849cfbeab78b23ec8e811ace71
dia_zx <dia_zx@mail.ru>
2024-09-23 01:58:19 +0300

    in progress

---
+++ Lesson10/Servises/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Lesson10.Servises.Repositories
+{
+    public interface IRepository<T, TId>
+    {
+        int Add(T item);
+        int Update(T item, TId id);
+        T GetById(TId id);
+        Collection<T> GetAll();
+        int Delete(TId id);
+    }
+}
